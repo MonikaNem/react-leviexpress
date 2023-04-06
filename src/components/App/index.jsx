47d072c849cc4f3a40a0dd972166ade7acc438f2
Monika <dedvseved@seznam.cz>
2023-04-06 19:22:45 +0200

    implementace routeru

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-export const App = () => (
-  <>
-    <Header />
-    <Home />
-    <Footer />
-  </>
-);
+const router = createBrowserRouter([
+  {
+    path:'/',  
+    element: <Home />,
+  }, 
+  {
+    path:'/reservation',  
+    element: <Reservation />,
+  }
+]);
+
+  export const App = () => (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  );
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { JourneyPicker } from '../JourneyPicker';
 
+
 export const Home = () => (
   <main>
     <JourneyPicker />
   </main>
 );
+
